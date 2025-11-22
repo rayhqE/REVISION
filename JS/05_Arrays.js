0ea6abd7bc24d736b1006ca1014c0220e19d5f47
Rayyan <rayhq3@gmail.com>
2025-11-22 23:46:00 +0530

    Arrays

---
+++ JS/05_Arrays.js
@@ -0,0 +1,15 @@
+const myArr = [0, 3, 4, true, "Rayyan"];
+const numsArr = [1, 2, 3, 4, 5];
+const heroesArr = ["thor", "ironman", "batman"];
+const arr2 = new Array(1, 3, 4, 5, 4);
+
+console.log(myArr[4]);
+
+//ARRAY METHODS
+
+// numsArr.push(6);
+// numsArr.push(7);
+// numsArr.pop()
+numsArr.unshift(9)
+numsArr.shift()
+console.log(numsArr);
