300c8a514f35cf44a2867645dba5442bcef16100
Rayyan <rayhq3@gmail.com>
2025-11-29 15:55:51 +0530

    Object destructuring completed

---
+++ JS/09_Object_Destructuring.js
@@ -0,0 +1,31 @@
+const repo = {
+  repoName: "Javascript revision",
+  repoCommits: 99,
+  repoOwner: "rayyan",
+};
+
+// repo.repoOwner
+
+const { repoCommits, repoName, repoOwner } = repo;
+
+
+console.log(repoCommits);
+console.log(repoName);
+// console.log(cum);
+
+
+//+++++++++++API FORMAT++++++++++++++++++
+// {
+//   "name":"rayyan",
+//   "age": 21,
+//   "address": "delhi",
+//   "email":"abc@email.com",
+//   "isEmployed":false
+// }
+
+
+[
+  {},
+  {},
+  {}
+]
