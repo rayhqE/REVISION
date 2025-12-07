29672680581123fb48afd249a94fac8bbd52c807
Rayyan <rayhq3@gmail.com>
2025-12-07 19:21:47 +0530

    Control Flow

---
+++ JS/If Else.js
@@ -0,0 +1,15 @@
+//if
+
+const isUserLoggedIn = true;
+
+// if (2 != "2") {
+//   console.log("Executed");
+// }
+
+// >,<,>=,<=,==,!=,===
+
+const temperature = 41;
+if (temperature < 50) {
+  console.log("temperature's less than 50");
+}
+console.log("temp's more than 50");
