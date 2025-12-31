e60d586b590ec0f19d79925cdd0b953763d3624d
Rayyan <rayhq3@gmail.com>
2025-12-31 16:47:35 +0530

    Project 1 Done

--- JS/27_Projects/01_Color Changer/script.js
+++ JS/27_Projects/01_Color Changer/script.js
@@ -1,3 +1,9 @@
 const buttons = document.querySelectorAll(".button");
-// document.query
-console.log(buttons);
+const body = document.querySelector("body");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    body.style.backgroundColor = e.target.id;
+    //WE COULD HAVE WRITTEN MULTIPLE IFs FOR EACH BUTTON COLOR ...BUT THIS SOLVED IT.
+  });
+});
