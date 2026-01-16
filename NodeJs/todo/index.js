1d350e2e072465b7dc2cf668cfad0797f102006c
Rayyan <rayhq3@gmail.com>
2026-01-16 06:27:05 +0000

    fs module

---
+++ NodeJs/todo/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = "./tasks.json";
+
+if (command === "add") {
+  addTask(argument);
+} else if (command === "list") {
+  listTask();
+} else if (command === "remove") {
+  removeTask(parseInt(argument));
+} else {
+  console.log("Command not Found!");
+}
