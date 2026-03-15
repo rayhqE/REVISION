3671ab8a47c680e16e44433dc232ec8bd85d83c7
Rayyan <rayhq3@.com>
2026-03-15 16:10:46 +0530

    recap

---
+++ JS/recap.js
@@ -0,0 +1,46 @@
+// // let score = "34";
+
+// // console.log(typeof score);
+// // console.log(typeof (score));
+
+// // let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+
+// // let num = 33;
+// // let stringNum = String(num);
+// // console.log(stringNum);
+// // console.log(typeof stringNum);
+
+// const name = "Rayyan";
+// const repoCount = 50;
+
+// // console.log(name + repoCount);
+// // console.log(`My name is ${name} nd my repo count is ${repoCount}`);
+
+// const gameName = new String("hitesh-hc");
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("o"));
+
+// const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+// const gameName1 = new String("hitesh-hc");
+// const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+// const newStringoNE = "     Rayyan     "
+// console.log(newStringoNE);
+// console.log(newStringoNE.trim());
+const url = "http://hitesh.com/hitesh%20choudhary";
+// console.log(url.replace('%20','-'))
+// console.log(url.includes("hites%20h"));
+
+const gameName = new String("d_off_season");
+console.log(gameName.split("_"));
+
+const myArr = [1, 3, "hello", true];
+const numsArray = [1, 2, 3, 4, 5, 6];
