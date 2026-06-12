bae44ed44062f5f0345a4ca47cd7ac928eeb69a7
Rayyan <rayhq3@gmail.com>
2026-06-12 20:48:19 +0530

    added list and remove functionality

--- NodeJs/Todo/todo.js
+++ NodeJs/Todo/todo.js
@@ -1,11 +1,41 @@
 const fs = require("fs");
-const filePath = "./tasks.js";
+const filePath = "./tasks.json";
+const loadTask = () => {
+  try {
+    const dataBuffer = fs.readFileSync(filePath);
+    const dataJson = dataBuffer.toString();
+    return JSON.parse(dataJson);
+  } catch (error) {
+    return [];
+  }
+};
+const saveTasks = (tasks) => {
+  const dataJson = JSON.stringify(tasks);
+  fs.writeFileSync(filePath, dataJson);
+};
+const addTask = (task) => {
+  const tasks = loadTask();
+  tasks.push({ task });
+  saveTasks(tasks);
+  console.log("Task Added ", task);
+};
+
+const listTask = () => {
+  const tasks = loadTask();
+  tasks.forEach((task, index) => console.log(`${index + 1} - ${task.task}`));
+};
+
+const removeTask = (index) => {
+  const tasks = loadTask();
+  tasks.splice(index - 1, 1);
+  saveTasks(tasks);
+};
 
 const command = process.argv[2];
 const argument = process.argv[3];
 
 if (command === "add") {
-  addtask(argument);
+  addTask(argument);
 } else if (command === "list") {
   listTask();
 } else if (command === "remove") {
