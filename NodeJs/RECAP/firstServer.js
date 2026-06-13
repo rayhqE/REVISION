7d325b273678fc228b87b22c7ab06ebf49a56d28
Rayyan <rayhq3@gmail.com>
2026-06-13 23:46:03 +0530

    req/res

--- NodeJs/RECAP/firstServer.js
+++ NodeJs/RECAP/firstServer.js
@@ -1,9 +1,32 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.end(() => {
-    console.log("Welcome to my server");
-    process.exit();
-  });
+  // console.log(
+  //   "reqURL: ",
+  //   req.url,
+  //   "\n",
+  //   "reqMethod: ",
+  //   req.method,
+  //   "\n",
+  //   "reqHeader: ",
+  //   req.headers,
+  // );
+  res.setHeader("content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first Server</title></head>");
+  if (req.url === "/") {
+    res.write("<body><h1>Welcome to my server.</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (req.url.toLowerCase() === "/about") {
+    res.write("<body><h1>About page</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.write("<body><h1>Chutiye</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  // res.write("<body><h1>Welcome to my server.</h1></body>");
 });
 const port = 3000;
 server.listen(port, () => {
