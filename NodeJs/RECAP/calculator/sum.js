4f3cbc4abe7364a58b7dd0fa488ee54dc85936ef
Rayyan <rayhq3@gmail.com>
2026-06-15 15:04:30 +0530

    calculator

---
+++ NodeJs/RECAP/calculator/sum.js
@@ -0,0 +1,24 @@
+const sumRequeshandler = (req, res) => {
+  console.log("Inside Sum Request Handler.", req.url);
+  const body = [];
+  req.on("data", (chunk) => body.push(chunk));
+  req.on("end", () => {
+    const bodyStr = Buffer.concat(body).toString();
+    const params = new URLSearchParams(bodyStr);
+    const bodyObj = Object.fromEntries(params);
+    const result = Number(bodyObj.first) + Number(bodyObj.second);
+    console.log(result);
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+      <html>
+        <head><title>Calculator</title></head>
+        <body>
+        <h1>Your Sum is: ${result}</h1>        
+        </body>
+      </html>
+      `);
+    return res.end();
+  });
+};
+
+module.exports = sumRequeshandler;
