59532d925f307dbe6d16dc89ebd15451586cc362
Rayyan <rayhq3@gmail.com>
2026-06-16 20:38:54 +0530

    express deepdive

--- Express/init/app.js
+++ Express/init/app.js
@@ -1,5 +1,5 @@
 //Core Module
-const http = require("http");
+// const http = require("http");
 
 //External Module
 const express = require("express");
@@ -9,9 +9,17 @@ const handleRequest = require("./user");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+  console.log("Came in First middleware: ", req.url, req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Came in Second middleware: ", req.url, req.method);
+  res.send("<p>Welcome to Express.js</p>");
+});
 
 const PORT = 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is listening on Port:${PORT}`);
 });
