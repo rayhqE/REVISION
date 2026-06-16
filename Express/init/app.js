3a5e77ba977c496493f374b73d64db01de58dc9d
Rayyan <rayhq3@gmail.com>
2026-06-16 23:03:54 +0530

    handling routes based on method

--- Express/init/app.js
+++ Express/init/app.js
@@ -9,19 +9,19 @@ const handleRequest = require("./user");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
+app.get("/", (req, res, next) => {
   console.log("Came in First middleware: ", req.url, req.method);
-  res.send("<p>Came from first middleware</p>");
+  // res.send("<p>Came from first middleware</p>");
   next();
+});
+
+app.post("/submit-details", (req, res, next) => {
+  console.log("Came in Second middleware: ", req.url, req.method);
+  res.send("<p>Welcome to Express.js</p>");
 });
 app.use("/", (req, res, next) => {
   console.log("Came in Another MiddleWare middleware: ", req.url, req.method);
   res.send("<p>Came from Another middleware</p>"); //This will throw error cuz res.send() in the upper app.use have closed th response stream.
-});
-
-app.use("/submit-details", (req, res, next) => {
-  console.log("Came in Second middleware: ", req.url, req.method);
-  res.send("<p>Welcome to Express.js</p>");
 });
 
 const PORT = 3000;
