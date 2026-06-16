f575604f2e291a659260dc2c0f5150c8c3904418
Rayyan <rayhq3@gmail.com>
2026-06-16 22:53:34 +0530

    handling routes based on path

--- Express/init/app.js
+++ Express/init/app.js
@@ -9,12 +9,17 @@ const handleRequest = require("./user");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("Came in First middleware: ", req.url, req.method);
+  res.send("<p>Came from first middleware</p>");
   next();
 });
+app.use("/", (req, res, next) => {
+  console.log("Came in Another MiddleWare middleware: ", req.url, req.method);
+  res.send("<p>Came from Another middleware</p>"); //This will throw error cuz res.send() in the upper app.use have closed th response stream.
+});
 
-app.use((req, res, next) => {
+app.use("/submit-details", (req, res, next) => {
   console.log("Came in Second middleware: ", req.url, req.method);
   res.send("<p>Welcome to Express.js</p>");
 });
